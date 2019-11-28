f26d51879c385f3931c36e721891f7918bdb8352
Sebastián Aedo <sebastian.aedo29@gmail.com>
2019-11-28 20:40:31 -0300

    Updates

---
+++ precache-manifest.76eb76b8ae2178203f5fa29dd5a43886.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6da08f9bee6adb2b2a0931e742beb246",
+    "url": "/versus/index.html"
+  },
+  {
+    "revision": "d0bff1b9a1d2490ce60d",
+    "url": "/versus/static/css/main.fbbe2919.chunk.css"
+  },
+  {
+    "revision": "a22d5bba7f6231872cb1",
+    "url": "/versus/static/js/2.ae957da9.chunk.js"
+  },
+  {
+    "revision": "d0bff1b9a1d2490ce60d",
+    "url": "/versus/static/js/main.684206fa.chunk.js"
+  },
+  {
+    "revision": "78fdcee0d14ea6aa0982",
+    "url": "/versus/static/js/runtime-main.3681a4d8.js"
+  }
+]);

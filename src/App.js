3bb0b716cea2083ad4a765c6d80a831fbea4199c
Sebastián Aedo <sebastian.aedo29@gmail.com>
2019-11-28 20:29:59 -0300

    working version

--- src/App.js
+++ src/App.js
@@ -1,14 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import data from './data.json'
+import { Rating, rate_1vs1, expose } from 'ts-trueskill';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import ReactPlayer from 'react-player'
+import ResultsDialog from './ResultsDialog';
+
+
+const bands = new Set(data.reduce((acc, d) => {acc.push(d.band); return acc}, new Array()));
+const randInt = (max) => {
+  return Math.floor(Math.random()*max);
+}
+
+window.expose = expose;
+
+const randomChoice = () => {
+  let a, b;
+  do {
+    const songs = [...data];
+    a = songs.splice(randInt(songs.length), 1)[0];
+    b = songs.splice(randInt(songs.length), 1)[0];
+  } while (a.band === b.band);
+  return [a, b];
+}
+
+
 
 function App() {
+  const [votes, setVotes] = useState(
+    [...bands].reduce((acc, d) => {acc[d] = new Rating(); return acc}, {}))
+  const [numVotes, setNumVotes] = useState(0);
+
+  // modal of results
+  const [open, setOpen] = useState(false);
+
+  // Two groups choosed to fight
+  const [fighters, setFighters] = useState(randomChoice());
+  
+  const [videoWidth, setVideoWidth] = useState(675);
+
+  const updateScore = (winner, looser) => {
+    const winnerRate = votes[winner.band];
+    const looserRate = votes[looser.band];
+
+    const [newWinnerRate, newLooserRate] = rate_1vs1(winnerRate, looserRate);
+    const newVotes = {}
+    newVotes[winner.band] = newWinnerRate;
+    newVotes[looser.band] = newLooserRate;
+
+
+    setVotes({...votes, ...newVotes});
+    setNumVotes(numVotes + 1);
+    setFighters(randomChoice());
+  }
+
+
+  useEffect(() => {
+    const { innerWidth: windowWidth } = window;
+    if (windowWidth < videoWidth)
+      setVideoWidth(windowWidth - windowWidth*.05);
+  }, [])
+
+
+  let v = [...Object.keys(votes)]
+  console.log(v.sort((a, b) => expose(votes[b]) - expose(votes[a])))
+  console.log(votes)
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Test</p>
-      </header>
-    </div>
+    <Grid container className="App">
+      <Grid item xs={12} md={12} lg={6} className='fighter'>
+        <Button variant="contained" color="primary" onClick={() => updateScore(fighters[0], fighters[1])}>{fighters[0].title}</Button>
+        <ReactPlayer url={fighters[0].url} width={videoWidth + 'px'}/>
+      </Grid>
+      <Grid item xs={12} md={12} lg={6} className='fighter'>
+        <Button variant="contained" color="primary" onClick={() => updateScore(fighters[1], fighters[0])}>{fighters[1].title}</Button>
+        <ReactPlayer url={fighters[1].url} width={videoWidth + 'px'}/>
+      </Grid>
+      <Button 
+        variant='contained' 
+        color='primary' 
+        style={{marginTop: 20}}
+        onClick={() => setOpen(true)}
+        disabled={!(numVotes >= 20)}
+        >
+          {numVotes >= 20 ? 'Ver Resultados' : `Faltan ${20 - numVotes} votos`}
+      </Button>
+      <ResultsDialog open={open} handleOpen={setOpen} values={votes}/>
+
+      {/* <ol>
+      {v.map(d => (<li key={d}>{d}: {expose(votes[d]).toFixed(0)}</li>))}
+
+      </ol> */}
+    </Grid>
   );
 }
 
---
+++ src/ResultsDialog.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Divider from '@material-ui/core/Divider';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import CloseIcon from '@material-ui/icons/Close';
+import Slide from '@material-ui/core/Slide';
+import Dialog from '@material-ui/core/Dialog';
+import { Rating, rate_1vs1, expose } from 'ts-trueskill';
+
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function ResultsDialog(props) {
+  const { open, values, handleOpen } = props;
+  const rows = Object.keys(values).reduce((acc, d) => {
+    acc.push({ name: d, score: expose(values[d]) });
+    return acc;
+  }, []).sort((a, b) => b.score - a.score);
+  console.log(rows)
+
+  return (
+    <Dialog
+      fullScreen
+      open={open}
+      onClose={() => handleOpen(false)}
+      TransitionComponent={Transition}
+    >
+      <AppBar>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            onClick={() => handleOpen(false)}
+            aria-label="close"
+          >
+            <CloseIcon />
+          </IconButton>
+          <Typography variant="h6">Resultados</Typography>
+        </Toolbar>
+      </AppBar>
+      <div className='results-table'>
+      <Table style={{marginTop: '30px'}} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Banda</TableCell>
+            <TableCell align="right">Puntos</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.score.toFixed(0)}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      </div>
+    </Dialog>
+  );
+}
